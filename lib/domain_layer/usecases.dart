60be96d085041d476adb98907d501eeb887efda9
saulo andrioli <saulo.andrioli@gmail.com>
2024-09-12 19:50:30 -0300

    remove clean_data_usecase reference

--- lib/domain_layer/usecases.dart
+++ lib/domain_layer/usecases.dart
@@ -1,12 +1,11 @@
 library usecases;
 
-export 'usecases/empregos/emprego_data_load_usecase.dart';
 export 'usecases/empregos/emprego_add_usecase.dart';
+export 'usecases/empregos/emprego_data_load_usecase.dart';
 export 'usecases/empregos/emprego_delete_usecase.dart';
 export 'usecases/horas/horas_create_usecase.dart';
-export 'usecases/salarios/salario_create_usecase.dart';
-export 'usecases/registration/clean_data_usecase.dart';
 export 'usecases/registration/login_user_usecase.dart';
 export 'usecases/registration/register_user_usecase.dart';
-export 'usecases/vault/set_vault_data_usecase.dart';
+export 'usecases/salarios/salario_create_usecase.dart';
 export 'usecases/vault/reset_vault.dart';
+export 'usecases/vault/set_vault_data_usecase.dart';
