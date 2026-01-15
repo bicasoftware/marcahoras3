fa46d73e8f712b7f48e22ff76b5ba584ca58b81b
saulo andrioli <saulo.andrioli@gmail.com>
2026-01-15 12:15:29 -0300

    add new icons and edit splash screen

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -5,4 +5,5 @@ class AppImages {
 
   static const String logoSmall = "$_base/logo_small.png";
   static const String logoBigTransparent = "$_base/logo_big_trans.png";
+  static const String wavebg = "$_base/bg.svg";
 }
